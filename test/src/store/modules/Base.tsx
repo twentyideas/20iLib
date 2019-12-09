ece7e8e1253762d422651b4064b50d5592d8ec97
Shahan Kazi <shahan@zabaat.com>
2019-12-09 14:14:45 -0800

    Remove test folder

--- test/src/store/modules/Base.tsx
+++
@@ -1,8 +0,0 @@
-import { Store } from "../Store"
-export default class Base {
-   store?: Store = undefined
-   async init(store: Store) {
-       this.store = store
-       return Promise.resolve()
-   }
-}
